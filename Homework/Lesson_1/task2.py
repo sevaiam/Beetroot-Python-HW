69e5a18c84ce6c1ad87f8130a033e45169ae2b37
Seva Levytskyi <iam@seva.lv>
2022-11-10 03:56:37 +0800

    Homework lesson 1 update 1

--- Homework/Lesson_1/task2.py
+++ Homework/Lesson_1/task2.py
@@ -1,5 +1,13 @@
 def main():
-    pass
+    # 'hello world'
+    print("hello world")
+    # 'HI!'
+    print('H', 'I', sep='', end='!\n')
+    # print("hello world")
+    print('print("hello world")')
+    # Triangle from #'s
+    print()
+    print('#'*5, '#'*4, '#'*3, '#'*2, '#', sep='\n')
 
 
 if __name__ == "__main__":
--- Homework/Lesson_1/task3.py
+++ Homework/Lesson_1/task3.py
@@ -1,5 +1,6 @@
 def main():
-    pass
+    print('#'*9, '\n#\t\t#'*3, '\n', '#'*9, sep='', end='\n\n')
+    print('#\t\t#\n'*2, '#'*9, '\n#\t\t#'*2, sep='')
 
 
 if __name__ == "__main__":
